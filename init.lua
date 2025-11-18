744c2c1a50dd41f232bdfde21db04e8bd9ad3502
T.J. Wilder <tj.wilder@nationaldebtrelief.com>
2025-11-18 10:03:22 -0600

    Better Python LSP defaults

--- init.lua
+++ init.lua
@@ -676,7 +676,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -52,6 +52,7 @@ return {
       -- lint.linters_by_ft['ruby'] = nil
       -- lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft['python'] = { 'ruff' }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
